b4543dccb0828bb9bbc5f37d447731eff3467651
willian emboava <w.emboava.o@gmail.com>
2021-01-20 11:29:36 -0300

    create debounceTime function

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,18 @@
 import { log } from './utils/promise-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 
 import './utils/array-helpers.js';
 
-const operation1 = takeUntil(3, () => service.sumItems('2143').then(log).catch(log));
+// const operation1 = takeUntil(3, () => service.sumItems('2143').then(log).catch(log));
+// const operation2 = debounceTime(500, operation1);
+// OR
 
-document.querySelector('#myButton').onclick = operation1;
+const action = debounceTime(500, takeUntil(3, () =>
+	service
+		.sumItems('2143')
+		.then(log)
+		.catch(log))
+	);
+
+document.querySelector('#myButton').onclick = action;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -5,3 +5,11 @@ export const compose = (...fns) => (value) => fns.reduceRight((previousValue, fn
 export const pipe = (...fns) => (value) => fns.reduce((previousValue, fn) => fn(previousValue), value);
 
 export const takeUntil = (times, fn) => () => times-- > 0 && fn();
+
+export	const	debounceTime	=	(milliseconds,	fn)	=>	{
+  let timer = 0;
+  return	()	=>	{
+    clearTimeout(timer)
+    timer = setTimeout(fn,	milliseconds);
+  };
+};
