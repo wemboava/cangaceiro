7f62e404f66bf7322d8bbb462c8ba723a6fa65bf
willian emboava <w.emboava.o@gmail.com>
2021-01-20 11:20:11 -0300

    create takeUntil function

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,9 @@
 import { log } from './utils/promise-helpers.js';
-import	{	notasService	as	service	}	from	'./nota/service.js';	
+import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
 
 import './utils/array-helpers.js';
 
-document.querySelector('#myButton')
-	.onclick = () =>
-		service
-			.sumItems('2143')
-			.then(log)
-			.catch(log);
+const operation1 = takeUntil(3, () => service.sumItems('2143').then(log).catch(log));
+
+document.querySelector('#myButton').onclick = operation1;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -3,3 +3,5 @@ export const partialize = (fn, ...params) => fn.bind(null, ...params);
 export const compose = (...fns) => (value) => fns.reduceRight((previousValue, fn) => fn(previousValue), value);
 
 export const pipe = (...fns) => (value) => fns.reduce((previousValue, fn) => fn(previousValue), value);
+
+export const takeUntil = (times, fn) => () => times-- > 0 && fn();
