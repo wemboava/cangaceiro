44c060e332c3708d509f0659355c98ad7d6ac4ce
willian emboava <w.emboava.o@gmail.com>
2021-02-02 12:26:47 -0300

    added delay and retry

--- public/app/app.js
+++ public/app/app.js
@@ -1,18 +1,22 @@
-import { log } from './utils/promise-helpers.js';
-import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
-
+import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
+import { notasService as service } from './nota/service.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+
+
+const operations = pipe(
+	partialize(takeUntil, 3),
+	partialize(debounceTime, 500)
+);
 
 // const operation1 = takeUntil(3, () => service.sumItems('2143').then(log).catch(log));
 // const operation2 = debounceTime(500, operation1);
 // OR
 
-const action = debounceTime(500, takeUntil(3, () =>
-	service
-		.sumItems('2143')
+const action = operations(() =>
+	retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
 		.then(log)
-		.catch(log))
+		.catch(log)
 	);
 
 document.querySelector('#myButton').onclick = action;
--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -1,5 +1,23 @@
 export const handleStatus = (res) => (res.ok ? res.json() : Promise.reject(res.statusText));
 
+export const timeoutPromise = (milliseconds, promise) => {
+	const timeout = new Promise((resolve, reject) =>
+		setTimeout(() => reject(`Limite	da	operação	excedido	(limite:	${milliseconds}	ms)`), milliseconds)
+	);
+	return Promise.race([ timeout, promise ]);
+};
+
+export const delay = (milliseconds) => (data) =>
+	new Promise((resolve) => setTimeout(() => resolve(data), milliseconds));
+
+export const retry = (retries, milliseconds, fn) =>
+	fn().catch((err) => {
+		console.log(retries);
+		return delay(milliseconds)().then(
+			() => (retries > 1 ? retry(--retries, milliseconds, fn) : Promise.reject(err))
+		);
+	});
+
 export const log = (param) => {
 	console.log(param);
 	return param;
